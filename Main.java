807b9de7f2938d79ef57bd4ea1f15c44c2333d81
jayachristian29 <jayachristianlopez@gmail.com>
2026-03-04 15:48:55 +0000

    Administered Steps 1, 2, 3

--- Main.java
+++ Main.java
@@ -1,18 +1,31 @@
 // When you are ready to have your work checked, change the value below from N to Y
 // ReadyForSubmission=N
-// YOUR NAME:
+// YOUR NAME: Jaya Eckert
 // COLLABORATORS:
-// DATE:  
+// DATE:  2/25/26
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("My thoughts on the article are:");
+		System.out.println("My thoughts on the article are: ");
+		Main.printBio(); 
+
 		// Part 2: Invoking/calling method
 		
 	}
 	
+	// method header
+	// return type: void, method identifier: printBio
+	public static void printBio ()
+	{
+		// method body
+		System.out.println("1. Self-drving cars should priortize the safety of pedestrians.");
+		System.out.println("2. The car's algorithm should mainly consdider the maximum amount of people that can be saved, as well as saving younger over older people, howver that can be subjective.");
+		System.out.println("3. I think it should be the general decision of the public for deciding the algorithm because they'd be the ones affected if there were a fatal accident.");
+
+	}
+
 	// Part 1: Creating method definition
 	
 }
